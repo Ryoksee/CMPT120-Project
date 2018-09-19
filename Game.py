73defb366cc54377b011a4ffeb7e89722c36162a
Ryoksee <42847453+Ryoksee@users.noreply.github.com>
2018-09-19 14:44:33 -0400

    Zork Adventure Game for CMPT120-Project

---
+++ Game.py
@@ -0,0 +1,88 @@
+# CMP 120L 113
+# Ryan Piasecki
+# 9/18/2018
+###
+
+
+# Game Title
+print("\nMomentous"
+      "\n=========\n")
+
+# Introduction to the game
+print("\n...How long has it been..."
+      "\nMinutes..?...Hours..?..Days..?"
+      "\nSlowly, you begin to remember...\n")
+
+#Continue
+input("\n<Press Enter to continue>\n")
+
+# Character creation
+print("\nExplorer...what is your name?\n")
+name = input("\n Enter a name: ")
+
+# Beginning
+print(name,"...I see.\n")
+
+# Continue
+input("\n<Press Enter to continue>\n")
+
+#Narrator
+print("\n No matter your name, you will find it will not matter here.\n")
+
+# Continue
+input("\n<Press Enter to continue>\n")
+
+#Narrator
+print("\n YoUr FaTe WaS SeAlEd bEfOrE yOu eVeN BeGaN\n")
+
+# Continue
+input("\n<Press Enter to wake up>\n")
+
+#Narrator
+print("\n Your eyes open. All around you lies debris of some sort. Heaps of scrap rests all around you\n")
+
+# Continue
+input("\n<Press Enter to continue>\n")
+
+#Narrator
+print("\n You stand, moving past the wreckage to find a better view of your surroundings.\n")
+
+# Continue
+input("\n<Press Enter to scout>\n")
+
+#Narrator
+print("\n Behind you lays the remains of a ship, tattered across large sharp rocks which lay upon the crevices of the beach."
+      "\nIn front of you, lies a most peculiar sight.\n")
+# Continue
+input("\n<Press Enter to look closer>\n")
+
+#Narrator
+print("\n Lying in front of you, past the very edge of the beach, is a wall of trees so thick, so close to each other, that almost all light cannot reach inside."
+      "\n These trees surround all parts of the island, making it impossible to see unless venturing into the forest itself.\n")
+
+#P Continue
+input("\n<Press Enter to continue>\n")
+
+#Narrator
+print("\nSlowly, you lay your hand on the nearest tree."\
+      "\nThe only option appears to be venturing inside the forest"
+      "\nYour head is swelling with questions, but they must be quelled for now"
+      "\nTaking a deep breathe, you step into the forest, your figure dissapearing into a maze of green\n")
+
+# Continue
+input("\nPress Enter to continue\n")
+
+#Narrator
+print("\nBut you were being watched.\n")
+
+# Continue
+input("\nPress Enter to continue\n")
+
+#Narrator
+print("\nFor whatever you may find, good night, and good luck\n")
+
+# Continue
+input("\n<Press Enter to end the Demo>\n")
+
+# Demo Ending Credits
+print("\n Demo created by Ryan Piasecki\n")
